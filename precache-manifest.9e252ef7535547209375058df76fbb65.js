5ac2d0df824b36bf29a5ed777e84d315d68bb8ef
Shubham Ayodhyavasi <shubhamgupta279@gmail.com>
2021-02-23 23:59:29 +0530

    Updates

---
+++ precache-manifest.9e252ef7535547209375058df76fbb65.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1aec4ee75872231a68a1d15c0f63a8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97350af9b22d6391a4e0",
+    "url": "/static/css/main.bf1e5642.chunk.css"
+  },
+  {
+    "revision": "43837a2f62f6867635c2",
+    "url": "/static/js/2.cd1f9574.chunk.js"
+  },
+  {
+    "revision": "75304a605f9c03e664948edc1c5186bb",
+    "url": "/static/js/2.cd1f9574.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97350af9b22d6391a4e0",
+    "url": "/static/js/main.c04758c6.chunk.js"
+  },
+  {
+    "revision": "820d9b6fff880beacd76",
+    "url": "/static/js/runtime-main.7a85cd74.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
